b8c651f3942b4eb8e49e01aab9d698712b7be80b
Aleksandr-Shapovalov <gpocsta@gmail.com>
2020-10-25 18:29:38 +0200

    111

--- Day7_Tanks/Program.cs
+++ Day7_Tanks/Program.cs
@@ -15,26 +15,38 @@ namespace Day7_Tanks
             string Victory;
             Tank[] Pantera = new Tank[5];
             Tank[] T34 = new Tank[5];
-           
-            for (int i = 0; i < T34.Length; i++)
-            {
-                T34[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "T34");
-            }
 
-            for (int i = 0; i < T34.Length; i++)
+            try
             {
-                Pantera[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "Pantera");
-            }
 
-            for (int i = 0; i < 5; i++)
+                for (int i = 0; i < T34.Length; i++)
+                {
+                    T34[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "T34");
+                }
+
+                for (int i = 0; i < T34.Length; i++)
+                {
+                    Pantera[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "Pantera");
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    T34[i].Print();
+                    Pantera[i].Print(); ;
+                    Victory = T34[i] * Pantera[i];
+                    Console.WriteLine();
+                    Console.WriteLine($"Победил танк {Victory}");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+            catch
             {
-                T34[i].Print();
-                Pantera[i].Print(); ;
-                Victory = T34[i] * Pantera[i];
-                Console.WriteLine();
-                Console.WriteLine($"Победил танк {Victory}");
-                Console.WriteLine();
-                Console.WriteLine();
+                Console.WriteLine("Блок catch");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
             }
 
             Console.ReadKey();
