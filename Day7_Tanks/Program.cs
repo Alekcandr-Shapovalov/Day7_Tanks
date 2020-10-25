0db69b01fedd60cbb90ca3a1e50422aee8f88371
Aleksandr-Shapovalov <gpocsta@gmail.com>
2020-10-25 17:46:34 +0200

    Добавьте файлы проекта.

---
+++ Day7_Tanks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WordOfTanks;
+
+namespace Day7_Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string Victory;
+            Tank[] Pantera = new Tank[5];
+            Tank[] T34 = new Tank[5];
+           
+            for (int i = 0; i < T34.Length; i++)
+            {
+                T34[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "T34");
+            }
+
+            for (int i = 0; i < T34.Length; i++)
+            {
+                Pantera[i] = new Tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "Pantera");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                T34[i].Print();
+                Pantera[i].Print(); ;
+                Victory = T34[i] * Pantera[i];
+                Console.WriteLine();
+                Console.WriteLine($"Победил танк {Victory}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        protected string name;
+        protected int ammunition;
+        protected int armor;
+        protected int maneuver;
+        public Tank(int ammunition, int armor, int maneuver, string name)
+        {
+
+            this.ammunition = ammunition;
+            this.armor = armor;
+            this.maneuver = maneuver;
+            this.name = name;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Танк - " + name + "   Боекомплект - " + ammunition + " Броня - " + armor + " Маневреность - " + maneuver);           
+        }
+
+        public static string operator *(Tank T34, Tank Pantera)
+        {
+            if (T34.ammunition + T34.armor + T34.maneuver > Pantera.ammunition + Pantera.armor + Pantera.maneuver)
+            {
+                return T34.name;
+            }
+            else
+            {
+                return Pantera.name;
+            }
+        }
+    }
+}
